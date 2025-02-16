5aa1393160b0fc65a7dd2226fab7bc0baf91b3cf
aseemshaikhok <asimshaikh0394@gmail.com>
2025-02-15 20:43:19 -0600

    Updates on basics

--- SentinelOne Logger.ps1
+++ SentinelOne Logger.ps1
@@ -1,62 +1,99 @@
 ﻿#SentinelOne Debugger Tool
 
-#define file name``
+Write-Host "SentinelOne Debugger Script Started" -BackgroundColor Green -ForegroundColor Black
+
+#define file name
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 $filename = hostname
 $filename = 'SentinelOne_' + $filename + "_" + (Get-Date -format "MMMddyyyyHHmm") +'.log' #time
-New-Item -path $DesktopPath -name $filename -ItemType "file"
+New-Item -path $DesktopPath -name $filename -ItemType "file" | Select-Object DirectoryName
 $logfile = $DesktopPath+'\'+$filename
 
 # Define divider
 $divider  = '================================================================================='
 
 # Log Date & Time
-Add-Content $logfile -Value (Get-Date)
+Add-Content $logfile -Value ("SentinelOne " + (hostname) + " - " + (Get-Date | Out-String))
 Add-Content $logfile -Value (systeminfo)
 Add-Content $logfile -Value $divider
 
-# SentinelOne Details
-Add-Content $logfile -Value "SentinelOne Details"
-Add-Content $logfile -Value $divider
 
-# Check if SentinelOne Service is Installed
-$SentinelService = Get-Service -Name SentinelAgent -ErrorAction SilentlyContinue
-if ($SentinelService) {
-    Add-Content $logfile -Value "Agent Service Found: $($SentinelService.DisplayName) - Status: $($SentinelService.Status)"
-} else {
-    Add-Content $logfile -Value "Error: SentinelOne Agent Service Not Found!"
-}
+################## SentinelOne Info #########################
+Add-Content $logfile -Value "SentinelOne Details"
 
 # Fetching SentinelOne Agent JSON Data
+Write-Host "`Verifying if SentinelOne is installed" -BackgroundColor Green -ForegroundColor Black
 try {
-    $SentinelHelper = New-Object -ComObject 'SentinelHelper.1'
-    $data = $SentinelHelper.GetAgentStatusJSON() | ConvertFrom-Json
-    Add-Content $logfile -Value "Agent Status JSON:"
-    Add-Content $logfile -Value ($data | Out-String)
+    $data = (New-Object -ComObject 'SentinelHelper.1').GetAgentStatusJSON() | ConvertFrom-Json
+    Add-Content $logfile -Value "Agent Status JSON:" 
+    Add-Content $logfile -Value ($data | Out-String) -NoNewline
 } catch {
     Add-Content $logfile -Value "Error: Unable to fetch SentinelOne Agent Data. COM Object may not be registered."
 }
 
+
+# Check if SentinelOne Services and process is Installed
+Write-Host "Verifying if SentinelOne services and process are running" -BackgroundColor Green -ForegroundColor Black
+$SentinelService = Get-Service -Name Sentinel* -ErrorAction SilentlyContinue
+if ($SentinelService) {
+    Add-Content $logfile -Value "Agent Service Found: " -NoNewline
+    Add-Content $logfile -Value ($SentinelService | Format-Table | Out-String)
+    Add-Content $logfile -Value ((Get-Process "Sentinel*" | Select-Object ProcessName, CPU, PrivateMemorySize, TotalProcessorTime, StartTime | Format-Table -AutoSize | Out-String))
+} else {
+    Add-Content $logfile -Value "Error: SentinelOne Agent Service Not Found!"
+}
+
+
 # Ping SentinelOne Management URL
+Write-Host "Verifying if SentinelOne ip addresses work" -BackgroundColor Green -ForegroundColor Black
 if ($data.'mgmt-url') {
     $url = ($data.'mgmt-url' -split "https://")[-1]
-    Add-Content $logfile -Value "Network Connectivity Check"
-    
+    Add-Content $logfile -Value "Network Connectivity Check" -NoNewline
     $pingResult = Test-NetConnection -ComputerName $url -InformationLevel Detailed
-    Add-Content $logfile -Value ($pingResult | Out-String)
+    Add-Content $logfile -Value ($pingResult | Out-String) 
 } else {
     Add-Content $logfile -Value "Error: SentinelOne Management URL not found in agent data."
 }
 
+
 # WMI test to verify if WMI is functioning correctly
+Write-Host "Verifying WMI functionality" -BackgroundColor Green -ForegroundColor Black
 try {
     $wmiresponse = Get-WmiObject Win32_OperatingSystem -ErrorAction Stop | Out-String 
-    Add-Content $logfile -Value "WMI is functioning correctly."
+    Add-Content $logfile -Value "WMI is functioning correctly." -NoNewline
     Add-Content $logfile -Value ($wmiresponse)
 } catch {
     Add-Content $logfile -Value "Error: WMI is not responding. It may be corrupted or disabled."
 }
 
-#Add EventViewer last 10 event viewer logs
+
+#Certificate Test
+Write-Host "Verifying installed certificate" -BackgroundColor Green -ForegroundColor Black
+Add-Content $logfile -Value "Performing Certificate Check" -NoNewline
+$DigiCertGlobalRootCAThumbprint = 'A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436'
+
+#$DigiCertGlobalRootCA = Get-ChildItem -Recurse Cert:\LocalMachine\Root | Where-Object -Property Thumbprint -EQ $DigiCertGlobalRootCAThumbprint | Select-Object -First 1
+$DigiCertGlobalRootCA = Get-ChildItem -Recurse Cert:\LocalMachine\Root | Where-Object {$_.Thumbprint -match $DigiCertGlobalRootCAThumbprint} | Select-Object -First 1
+
+if(!$DigiCertGlobalRootCA){
+    Add-Content $logfile -Value 'ERROR: DigiCert Global Root (Thumbprint A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436) CA is not imported in the LocalMachine certificate store.'
+} else {
+    Add-Content $logfile -Value 'DigiCert Global Root CA is imported (Thunbprint A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436).' 
+}
+
+
+#Cipher Test
+
+
+
+#EventViewer last 10 event viewer logs
+Write-Host "Gathering SentinelOne agent event viewer logs" -BackgroundColor Green -ForegroundColor Black
+Add-Content $logfile -Value "SentinelOne Event Logs" -NoNewline
+try{
+    Add-Content $logfile -Value (Get-WinEvent -LogName 'SentinelOne/Operational' -MaxEvents 10 | Format-Table -Wrap | Out-String)
+} catch {
+    Add-Content $logfile -Value "SentinelOne event logs not found" 
+}
 
 Add-Content $logfile -Value $divider
+Add-Content $logfile -Value (systeminfo)
--- testcases.ps1
+++ testcases.ps1
@@ -13,7 +13,25 @@ New-Item -Path $logfile -ItemType File | Out-Null
 # Define divider
 $divider  = '================================================================================='
 
-#Look for Event-Viewer
-Add-Content $logfile -Value (Get-WinEvent -LogName 'SentinelOne/Operational' -MaxEvents 10 | Format-Table -Wrap | Out-String)
+
+# Fetching SentinelOne Agent JSON Data
+$procs = Get-Process | Sort-Object "WorkingSet"
+foreach($proc in $procs) {
+   $NonPagedMem = [int]($proc.NPM/1024)
+   $WorkingSet = [int64]($proc.WorkingSet64/1024)
+   $VirtualMem = [int]($proc.VM/1MB)
+   $id= $proc.Id
+   $machine = $proc.MachineName
+   $process = $proc.ProcessName
+   $procdata = new-object psobject
+   $procdata | Add-Member noteproperty NonPagedMem $NonPagedMem
+   $procdata | Add-Member noteproperty WorkingSet $WorkingSet 
+   $procdata | Add-Member noteproperty machine $machine
+   $procdata | Add-Member noteproperty process $process
+   $procdata | Select-Object machine,process,WorkingSet,NonPagedMem
+}
+
+
+
 Add-Content $logfile -Value $divider
 Add-Content $logfile -Value "Log Collection Completed"
