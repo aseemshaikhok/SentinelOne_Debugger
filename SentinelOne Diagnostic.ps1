8d28989f9a6025112c95a78182c757a1f8d896ba
aseemshaikhok <asimshaikh0394@gmail.com>
2025-02-18 16:06:07 -0600

    Updates to machine info

--- SentinelOne Diagnostic.ps1
+++ SentinelOne Diagnostic.ps1
@@ -1,6 +1,6 @@
-﻿#SentinelOne Debugger Tool
+﻿#SentinelOne Diagnostic Tool
 
-Write-Host "SentinelOne Debugger Script Started" -BackgroundColor Green -ForegroundColor Black
+Write-Host "SentinelOne Diagnostic Script Started" -BackgroundColor Green -ForegroundColor Black
 
 #define file name
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
@@ -37,8 +37,8 @@ Write-Host "Verifying if SentinelOne services and process are running" -Backgrou
 $SentinelService = Get-Service -Name Sentinel* -ErrorAction SilentlyContinue
 if ($SentinelService) {
     Add-Content $logfile -Value "Agent Service Found: " -NoNewline
-    Add-Content $logfile -Value ($SentinelService | Format-Table | Out-String)
-    Add-Content $logfile -Value ((Get-Process "Sentinel*" | Select-Object ProcessName, CPU, PrivateMemorySize, TotalProcessorTime, StartTime | Format-Table -AutoSize | Out-String))
+    Add-Content $logfile -Value ($SentinelService | Format-Table -Wrap | Out-String)
+    Add-Content $logfile -Value ((Get-Process "Sentinel*" | Select-Object ProcessName, CPU, PrivateMemorySize, TotalProcessorTime, StartTime | Format-Table -Wrap | Out-String))
 } else {
     Add-Content $logfile -Value "Error: SentinelOne Agent Service Not Found!"
 }
@@ -78,12 +78,11 @@ $DigiCertGlobalRootCA = Get-ChildItem -Recurse Cert:\LocalMachine\Root | Where-O
 if(!$DigiCertGlobalRootCA){
     Add-Content $logfile -Value "ERROR: DigiCert Global Root (Thumbprint A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436) CA is not imported in the LocalMachine certificate store.`n"
 } else {
-    Add-Content $logfile -Value "DigiCert Global Root CA is imported (Thunbprint A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436).`n" 
+    Add-Content $logfile -Value "DigiCert Global Root CA is imported (Thumbprint A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436).`n" 
 }
 
 
 #Cipher Test
-
 
 
 #EventViewer last 10 event viewer logs
@@ -98,27 +97,50 @@ if ($eventLogs){
 
 Add-Content $logfile -Value $divider
 
+
+######################### SentinelOne Installation Info #########################
+Write-Host "SentinelOne Installation Details" -BackgroundColor Green -ForegroundColor Black
+
+#Read sc-exit code
+$eventLogs = Get-Content -Path "C:\Windows\Temp\sc-exit-code.txt"
+Add-Content $logfile -Value ("Sentinel Installer run exit code:" + $eventLogs)
+$eventLogs
+
+#Read etl logs. 
+
+
 ######################### Machine Info #########################
 
-#disk information
+#Disk information
 Add-Content $logfile -Value ("Disk Information") -NoNewline
-Add-Content $logfile -Value (Get-Volume | Select-Object DriveLetter, FileSystem, FileSystemLabel, SizeRemaining, Size | Format-Table -AutoSize | Out-String)
+Add-Content $logfile -Value (Get-Volume | Select-Object DriveLetter, FileSystem, FileSystemLabel, SizeRemaining, Size | Format-Table -Wrap | Out-String)
 
-#memory information
+
+#Memory information
 Add-Content $logfile -Value ("Memory Information") -NoNewline
 $memory = Get-CimInstance Win32_OperatingSystem | Select-Object @{Name="Total Memory (GB)"; Expression={[math]::round($_.TotalVisibleMemorySize / 1MB, 2)}},
                                                               @{Name="Free Memory (GB)"; Expression={[math]::round($_.FreePhysicalMemory / 1MB, 2)}},
                                                               @{Name="Total Virtual Memory (GB)"; Expression={[math]::round($_.TotalVirtualMemorySize / 1MB, 2)}},
                                                               @{Name="Free Virtual Memory (GB)"; Expression={[math]::round($_.FreeVirtualMemory / 1MB, 2)}}
+Add-Content $logfile -Value ($memory | Format-Table -Wrap| Out-String)
 
-Add-Content $logfile -Value ($memory | Format-Table -AutoSize| Out-String)
 
-#hotfix information
+#Hotfix information
 Add-Content $logfile -Value ("Hotfix Installed") -NoNewline
-Add-Content $logfile -Value (Get-HotFix | Select-Object HotFixID, InstalledOn, Description, InstalledBy | Sort-Object InstalledOn -Descending | Format-Table -AutoSize| Out-String)
+Add-Content $logfile -Value (Get-HotFix | Select-Object HotFixID, InstalledOn, Description, InstalledBy | Sort-Object InstalledOn -Descending | Format-Table -Wrap| Out-String)
+
 
 #Application logs - SentinelOne search
+Write-Host "Generating Application logs" -BackgroundColor Green -ForegroundColor Black
+$eventLogs = Get-EventLog -LogName Application | Where-Object { $_.Message -match "Sentinel" } | Select-Object -First 10 
 
-
+if ($eventLogs) {
+    Add-Content $logfile -Value ($eventLogs | Format-Table -Wrap | Out-String)
+} else  {Add-Content $logfile -Value ("Sentinel events not found in application logs")}
 
 #System logs - SentinelOne search
+Write-Host "Generating System logs" -BackgroundColor Green -ForegroundColor Black
+$eventLogs = Get-EventLog -LogName System | Where-Object { $_.Message -match "Sentinel" } | Select-Object -First 10 
+if ($eventLogs) {
+    Add-Content $logfile -Value ($eventLogs | Format-Table -Wrap | Out-String)
+}
